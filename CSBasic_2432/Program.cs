2761fe093ebfb09af7835347ab5697764812e379
Gihun Ham <ham@e-mirim.hs.kr>
2024-04-12 09:54:56 +0900

    #6 오버플로우 해결

--- CSBasic_2432/Program.cs
+++ CSBasic_2432/Program.cs
@@ -17,6 +17,11 @@ namespace CSBasic_2432
             int a = 2000000000;  // 20억
             int b = 1000000000;  // 10억
             Console.WriteLine(a + b);
+
+            // 오버플로우 연습2
+            Console.WriteLine((long)a + b);
+            Console.WriteLine(a + (long) b);
+            Console.WriteLine((long)a + (long)b);
         }
     }
 }
